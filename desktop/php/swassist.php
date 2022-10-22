298eb3a848e5cf9415184dbf2894070b475094db
Christian <chris@jeedomide1.khome.local>
2022-10-22 11:56:46 +0200

    equipements en tableau

--- desktop/php/swassist.php
+++ desktop/php/swassist.php
@@ -31,6 +31,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
             <input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>
             <div class="input-group-btn">
                 <a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i></a>
+		<a id="bt_pluginDisplayAsTable" class="btn roundedRight hidden" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
             </div>
         </div>
         <!-- Liste des équipements du plugin -->
